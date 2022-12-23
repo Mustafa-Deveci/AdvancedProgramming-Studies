706646791eea8b4251302df525de90ed40e1e4b9
Mustafa Deveci <mustafa.deveci@stu.fsm.edu.tr>
2022-12-23 21:10:56 +0300

    Update sw.js

--- TermProjectSnake/sw.js
+++ TermProjectSnake/sw.js
@@ -1,16 +1,18 @@
-function save(req, resp) {
-  return caches.open(CACHE)
-  .then(cache => {
-    cache.put(req, resp.clone());
-    return resp;
-  }) 
-  .catch(console.log)
-}
-function fetchCB(e) { //fetch first
-  let req = e.request
-  e.respondWith(
-    fetch(req).then(r2 => save(req, r2))
-    .catch(() => { return caches.match(req).then(r1 => r1) })
-  )
-}
-self.addEventListener('fetch', fetchCB)
+async function save(req, resp) {
+    try {
+        const cache = await caches.open(CACHE);
+        cache.put(req, resp.clone());
+        return resp;
+    } catch (data) {
+        return console.log(data);
+    }
+  }
+  function fetchCB(e) { //fetch first
+    let req = e.request
+    e.respondWith(
+      fetch(req).then(r2 => save(req, r2))
+      .catch(async () => { const r1 = await caches.match(req);
+      return r1; })
+    )
+  }
+  self.addEventListener('fetch', fetchCB)
